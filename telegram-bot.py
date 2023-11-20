9f23c5469e518b16ce67a943d283957b028049f1
Breandon Chien <breandon@Breandons-MacBook-Pro.local>
2023-11-20 21:15:34 +0800

    update

--- telegram-bot.py
+++ telegram-bot.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 from parsel import Selector
 from clever_chat import Client
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater,  Application, filters, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, ContextTypes
+from telegram.ext import Updater, Application, filters, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, ContextTypes
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 TOKEN: Final = '6979522872:AAE7oPHzrlqbfuXCXFiIW_95qmoOx1R5dyQ'
 BOT_USERNAME: Final = 'breancbot'
-ENTER_POSTAL, PROCESS_QUERY = range(2)
+ENTER_POSTAL, PROCESS_QUERY, FILTER_REST,  CHAT_MESSAGE = range(4)
 
 # Commands
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -25,6 +25,10 @@ async def reccos_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reccoFlag = True
     await update.message.reply_text('Sure, please enter your postal code and I\'ll recommend something!')
     return ENTER_POSTAL
+
+async def chat_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Sure, let\'s talk about something!')
+    return CHAT_MESSAGE
 
 async def quit_command(update, context):
     await update.message.reply_text('Sure, good bye!')
@@ -57,23 +61,53 @@ async def process_query(update, context):
     else: 
         await query.edit_message_text("Selected option: Open anytime")
     url = (queryGenerator(isFiltered, context.user_data["postal"]))
-    print(url)
     #Scrape results
     options = Options()
     options.add_argument('--headless=new')
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    results = "How about the following:\n"
     page_content = driver.page_source
     response = Selector(page_content)
-    for el in response.xpath('//div[contains(@aria-label, "Results for")]/div/div[./a]'):
-                            
-        results = results + (el.xpath('./a/@aria-label').extract_first('')) + "\n"
-        results = results + el.xpath('./a/@href').extract_first('')+ "\n"
-        results = results + "\n"
+    dict = {'All':[]}
+    for el in response.xpath('//div[contains(@aria-label, "Results for")]/div/div[./a]'):  
+        #construct result              
+        name = (el.xpath('./a/@aria-label').extract_first(''))
+        link =  el.xpath('./a/@href').extract_first('')
+        priceRange = el.xpath('./div[2]/div[4]/div[1]/div/div/div[2]/div[3]/div/span[3]/span[2]/text()').extract_first('')
+        if priceRange == '':
+            priceRange = "No price range yet"
+        type = el.xpath('./div[2]/div[4]/div[1]/div/div/div[2]/div[4]/div[1]/span[1]/span/text()').extract_first('')
+        reviews = el.xpath('./div[2]/div[4]/div[1]/div/div/div[2]/div[3]/div/span[2]/span/@aria-label').extract_first('')
+        if reviews == '':
+            reviews = "No reviews yet"
+        result =  { "name":name, "link":link, "priceRange":priceRange, "type":type,"reviews":reviews}
+        if type not in dict:
+            newList = [result]
+            dict[type] = newList
+        else: 
+            dict[type].append(result)
+        dict['All'].append(result)
     driver.close()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text= results)
-    return ConversationHandler.END
+
+    keyboard = []
+    for key, value in dict.items():
+        keyboard.append([InlineKeyboardButton(key, callback_data=key)])
+    context.user_data["results"] = dict
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Filter by restaurant type:", reply_markup=reply_markup)
+    return FILTER_REST
+
+async def filter_restaurant(update, context):
+    query = update.callback_query
+    type = query.data
+    await query.edit_message_text("Selected option: " + type)
+    results = "How about the following:\n"
+    for item in context.user_data["results"][type]:
+        results = results + '<a href="{0}">{1}</a>'.format(item['link'], item['name']) + " - " + item['priceRange'] + "\n"
+        results = results + item['type'] + "\n" + item['reviews'] + "\n"
+        results = results + "\n"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text= results, parse_mode = 'html')
+    return ENTER_POSTAL
 
 def queryGenerator(isFiltered, postal):
 
@@ -83,8 +117,7 @@ def queryGenerator(isFiltered, postal):
     list = ["https://www.google.com/maps/search/best+food+"+additionalFilter,
             "https://www.google.com/maps/search/what+to+eat+"+additionalFilter,
             "https://www.google.com/maps/search/good+food+"+additionalFilter,
-            "https://www.google.com/maps/search/whats+nice+to+eat+"+additionalFilter,
-            "https://www.google.com/maps/search/+food+"+additionalFilter]
+            "https://www.google.com/maps/search/+restaurants+"+additionalFilter]
     return  random.choice(list)
 
 def validatePostal(postal):
@@ -113,21 +146,15 @@ async def handle_message(update: Update, context:ContextTypes.DEFAULT_TYPE):
     if message_type == 'group':
         if BOT_USERNAME in text:
             new_text: str = text.replace(BOT_USERNAME,'').strip()
-            response: str = handle_response(new_text)
+            response: Client.get_response(text)
         else:
             return
     else:
-        response: str = handle_response(update, text)
+        response: str =  Client.get_response(text)
 
     print('Bot:', response)
     await update.message.reply_text(response)
-
-
-def handle_response(update: Update, text: str) -> str:
-    processed: str = text.lower()
-    if '/quit' == processed:
-        return 'You have not selected recommendation mode.'
-    return Client.get_response(processed)
+    return CHAT_MESSAGE
 
 
 #error handling
@@ -143,20 +170,25 @@ if __name__=='__main__':
     #Commands
     app.add_handler(CommandHandler('help', help_command))
     
-    conv_handler = ConversationHandler(
+    recco_handler = ConversationHandler(
         entry_points=[CommandHandler('reccos', reccos_command)], 
         states={
             ENTER_POSTAL: [MessageHandler(filters.TEXT & ~filters.COMMAND, enter_postal)],
-            PROCESS_QUERY: [CallbackQueryHandler(process_query)]
+            PROCESS_QUERY: [CallbackQueryHandler(process_query)],
+            FILTER_REST: [CallbackQueryHandler(filter_restaurant)]
         },
         fallbacks=[CommandHandler('quit', quit_command)]
     )
+    app.add_handler(recco_handler)
 
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('chat', chat_command)], 
+        states={
+            CHAT_MESSAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)],
+        },
+        fallbacks=[CommandHandler('quit', quit_command)]
+    )
     app.add_handler(conv_handler)
-
-    #messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
-
     #error
     app.add_error_handler(error)
 
